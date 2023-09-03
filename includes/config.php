b86817bb3420ec03ba34a8081b156023e4a1bc8f
Mugendi01 <mugendiireri@zetech.ac.ke>
2023-09-03 19:38:10 +0300

    Compiler library WMS

---
+++ includes/config.php
@@ -0,0 +1,18 @@
+<?php
+/*
+|--------------------------------------------------------------------------
+| -WMS V2
+|--------------------------------------------------------------------------
+| Author: Michael Mugendi
+| Project Name: WMS
+| Version: v2
+
+|
+|
+*/
+  define( 'DB_HOST', 'localhost' );          // Set database host
+  define( 'DB_USER', 'root' );             // Set database user
+  define( 'DB_PASS', '' );             // Set database password
+  define( 'DB_NAME', 'WMS' );        // Set database name
+
+?>
